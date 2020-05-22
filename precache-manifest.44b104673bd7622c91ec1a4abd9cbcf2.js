f89118e6ae6a8cc95da66e64e94f7845360c5988
vjlicious <viju1997@gmail.com>
2020-05-22 18:37:28 +0530

    Updates

---
+++ precache-manifest.44b104673bd7622c91ec1a4abd9cbcf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b5622ed8b353ae8af7af58426cce08",
+    "url": "/Guess-The-Pokemon/index.html"
+  },
+  {
+    "revision": "74993b301c986561573f",
+    "url": "/Guess-The-Pokemon/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "754375edeb69d8a6dc38",
+    "url": "/Guess-The-Pokemon/static/js/2.08034478.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/Guess-The-Pokemon/static/js/2.08034478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74993b301c986561573f",
+    "url": "/Guess-The-Pokemon/static/js/main.82f5230f.chunk.js"
+  },
+  {
+    "revision": "149c83bfd67c4a198510",
+    "url": "/Guess-The-Pokemon/static/js/runtime-main.24b96cfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Guess-The-Pokemon/static/media/logo.5d5d9eef.svg"
+  }
+]);
